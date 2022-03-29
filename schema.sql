9f51ea3932e152a54b2025631baebc1d23579a21
Александр <aleksder7708@yandex.ru>
2022-03-29 20:41:48 +0500

    Еще ряд правок

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ yeticave_67;
 CREATE TABLE users
 (
   id          INT AUTO_INCREMENT PRIMARY KEY,
-  create_data DATETIME DEFAULT CURRENT_TIMESTAMP,
+  create_datе DATETIME DEFAULT CURRENT_TIMESTAMP,
   email       VARCHAR(256) NOT NULL UNIQUE,
   name        VARCHAR(128) NOT NULL,
   password    VARCHAR(128) NOT NULL,
@@ -26,14 +26,14 @@ CREATE TABLE categories
 CREATE TABLE lots
 (
   id          INT AUTO_INCREMENT PRIMARY KEY,
-  create_data DATETIME DEFAULT CURRENT_TIMESTAMP,
+  create_datе DATETIME DEFAULT CURRENT_TIMESTAMP,
   title       VARCHAR(128) NOT NULL,
   description TEXT         NOT NULL,
-  url_image   VARCHAR(128),
-  price        INT          NOT NULL,
-  finish_date DATETIME,
+  image       VARCHAR(300) NOT NULL,
+  price       INT          NOT NULL,
+  finish_date DATETIME     NOT NULL,
   step        INT          NOT NULL,
-  user_id     INT,
+  user_id     INT          NOT NULL,
   winner_id   INT,
   category_id INT,
   FOREIGN KEY (user_id) REFERENCES users (id),
@@ -44,10 +44,10 @@ CREATE TABLE lots
 CREATE TABLE bets
 (
   id          INT AUTO_INCREMENT PRIMARY KEY,
-  create_data DATETIME DEFAULT CURRENT_TIMESTAMP,
-  price  INT NOT NULL,
-  user_id     INT,
-  lot_id      INT,
+  create_datе DATETIME DEFAULT CURRENT_TIMESTAMP,
+  price       INT NOT NULL,
+  user_id     INT NOT NULL,
+  lot_id      INT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id),
   FOREIGN KEY (lot_id) REFERENCES lots (id)
 );
