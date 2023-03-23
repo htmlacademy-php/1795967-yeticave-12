d474ea574bed38c55e002a927ccdb2ebca632cae
aleks <aleksder7708@yandex.ru>
2023-03-23 20:38:35 +0500

    Исправления 23.03

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE yeti_cave_67
-	DEFAULT CHARACTER SET utf8
- 	DEFAULT COLLATE utf8_general_ci;
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
 
 USE yeti_cave_67;
 
@@ -10,13 +10,13 @@ CREATE TABLE users
   date_create DATETIME DEFAULT CURRENT_TIMESTAMP,
   email       VARCHAR(256) NOT NULL UNIQUE,
   name        VARCHAR(128) NOT NULL,
-  password    VARCHAR(128) NOT NULL,
+  password    VARCHAR(256) NOT NULL,
   contact     VARCHAR(256)
 );
 
 CREATE TABLE categories
 (
-  id   INT AUTO_INCREMENT PRIMARY KEY,
+  id   SMALLINT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(128) NOT NULL UNIQUE,
   code VARCHAR(128) NOT NULL UNIQUE
 );
@@ -24,16 +24,16 @@ CREATE TABLE categories
 CREATE TABLE lots
 (
   id          INT AUTO_INCREMENT PRIMARY KEY,
-  date_create DATETIME DEFAULT CURRENT_TIMESTAMP,
-  title       VARCHAR(128) NOT NULL,
-  description TEXT         NOT NULL,
-  image       VARCHAR(300) NOT NULL,
-  price       INT          NOT NULL,
-  finish_date DATETIME     NOT NULL,
-  step        INT          NOT NULL,
-  user_id     INT          NOT NULL,
+  date_create DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  title       VARCHAR(128)                       NOT NULL,
+  description TEXT                               NOT NULL,
+  image       VARCHAR(300)                       NOT NULL,
+  price       MEDIUMINT                          NOT NULL,
+  finish_date DATETIME                           NOT NULL,
+  step        SMALLINT                           NOT NULL,
+  user_id     INT                                NOT NULL,
   winner_id   INT,
-  category_id INT          NOT NULL,
+  category_id INT                                NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id),
   FOREIGN KEY (winner_id) REFERENCES users (id),
   FOREIGN KEY (category_id) REFERENCES categories (id)
@@ -50,6 +50,6 @@ CREATE TABLE bets
   FOREIGN KEY (lot_id) REFERENCES lots (id)
 );
 
-CREATE FULLTEXT INDEX lots_ft ON lots(title,description);
+CREATE FULLTEXT INDEX lots_ft ON lots (title, description);
 
 
--- tests/Unit/functions/template/TimeLeftTest.php
+++ tests/Unit/functions/template/TimeLeftTest.php
@@ -3,24 +3,19 @@
 
 use PHPUnit\Framework\TestCase;
 
-require_once __DIR__ . '/../../../../functions/template.php';
 class TimeLeftTest extends TestCase
 {
 
-    public function testTimeLeft()
+    /**
+     * Метод тестирует функцию TimeLeft
+     * @var string $finisDate
+     * @return void
+     */
+    public function testTimeLeft(): void
     {
-//        $currentDate = '2022-03-01 12:30';
         $finishDate = '2022-03-02';
         $result = timeLeft($finishDate);
         $this->assertEquals(["00", "00"], $result);
-
-//        $finishDate = '2022-03-03';
-//        $result = timeLeft($finishDate);
-//        $this->assertEquals(["35", "30"], $result);
-//
-//        $finishDate = '2022-02-03';
-//        $result = timeLeft($finishDate);
-//        $this->assertEquals(["00", "00"], $result);
     }
 
 }
